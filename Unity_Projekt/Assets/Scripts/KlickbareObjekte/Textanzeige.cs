4f47cd46e2a0384f3810a3deb377e2d923e193d7
Kunstiception <lukas-bohl.de@outlook.de>
2024-01-21 23:38:47 +0100

    Weitere Arbeit an der neuen Textanzeige
    Es wurde weiter an der neuen Textanzeige gearbeitet.

--- Unity_Projekt/Assets/Scripts/KlickbareObjekte/Textanzeige.cs
+++ Unity_Projekt/Assets/Scripts/KlickbareObjekte/Textanzeige.cs
@@ -2,25 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using Unity.VisualScripting;
 
 public class Textanzeige : MonoBehaviour
 {
-    public GameObject[] box;
-    public Text[] descriptions;
-    public Text textanzeige;
-    private GameObject activeBox;
-    public GameObject gameManager;
+    /// <summary>
+    /// Die auszugenden Texte
+    /// </summary>
+    public GameObject description;
+    /// <summary>
+    /// Die gerade aktive Beschreibung
+    /// </summary>
+    private GameObject activeDescription;
+    /// <summary>
+    /// Das geklickte Item, über dem die Beschreibung erscheinen soll
+    /// </summary>
+    private GameObject clickedItem;
+    /// <summary>
+    /// Die Objekte, die geklickt werden können
+    /// </summary>
+    public GameObject[] clickableItems;
+    /// <summary>
+    /// Die Position des geklickten Objekts
+    /// </summary>
+    public Vector3 clickedItemPosition;
+    /// <summary>
+    /// Referenz zur Main Camera
+    /// </summary>
+    public Camera mainCamera;
+    //mit Hilfe von: https://docs.unity3d.com/ScriptReference/Camera.WorldToScreenPoint.html
+    /// <summary>
+    /// Die Kamera
+    /// </summary>
+    private Camera camera;
+   
+    void Start()
+    {
+        // Kamera-Komponente holen
+        camera = mainCamera.GetComponent<Camera>();
+        // Beschreibung erst mal deaktivieren
+        description.SetActive(false);
+    }
 
     public void SetDescription(int index)
     {
-        if (index >= 0 && index <= descriptions.Length - 1)
-            gameObject.SetActive(box[index]);
+        if (index >= 0 && index <= clickableItems.Length - 1)
+        {
+            // Beschreibung nach Klick auf Lupensmybol aktivieren
+            description.SetActive(true);
             
-        activeBox = box[index];
+            // Diese Beschreibung ist die aktive Beschreibung
+            activeDescription = description; 
 
-        activeBox.GetComponent<Animator>().SetBool("active", true);
-        //https://docs.unity3d.com/Packages/com.unity.visualscripting@1.7/manual/vs-variables-reference.html
-        //activeBox.transform.position = Variables.Scene(GameManagerInScene).gameObject.ClickedObject.transform.position;
+            // Das klickbare Item wird über den Index ausgewählt
+            clickedItem = clickableItems[index];
+
+            // Die Position des geklickten Items ermitteln
+            clickedItemPosition = clickedItem.transform.position;
+
+            // mit Hilfe von: https://docs.unity3d.com/ScriptReference/Camera.WorldToScreenPoint.html
+            // Die aktive Beschreibung wird mit einem leichten Abstand zum geklickten Item gesetzt
+            activeDescription.transform.position = camera.WorldToScreenPoint(clickedItemPosition) + new Vector3(0f, 200f, 0f);
+        }
     }
 }
